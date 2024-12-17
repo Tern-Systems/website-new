2cf5e75f051088ec945175545eb44958eb152c09
Maksym Shapovalov <maksym.shapovalovv@gmail.com>
2024-12-17 15:50:43 -0500

    fix: fix page mapping in Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['index.tsx'],
+    pageExtensions: ['page.tsx', 'not-found.tsx', '_app.tsx'],
     images: { // TODO
         remotePatterns: [
             {
