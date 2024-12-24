ad86fffc1bb90b78c1e362a24a6e55ee4f2d6f1b
Jomar Tabios Granada <jomart.granada@gmail.com>
2024-12-24 21:55:40 +0800

    feat: add invoice history on manage subscriptions page

--- src/pages/Profile/Billing/ManageSubscriptions/index.page.tsx
+++ src/pages/Profile/Billing/ManageSubscriptions/index.page.tsx
@@ -3,6 +3,8 @@ import {ReactSVG} from "react-svg";
 import Image from "next/image";
 import axios from "axios";
 
+import {Invoice} from "@/app/types/billing";
+
 import {Subscription} from "@/app/ui/templates/PaymentMethodTool";
 import {Route} from "@/app/static";
 
@@ -12,6 +14,7 @@ import {useModal, useUser} from "@/app/context";
 
 import {ScrollEnd} from "@/app/ui/misc";
 import {BaseModal} from "@/app/ui/modals";
+import {ExportInvoiceModal} from "../PurchasingInformation/ExportInvoiceModal";
 import {FullScreenLayout} from "@/app/ui/layout";
 import {Button, Select} from "@/app/ui/form";
 import {CancelModal} from "./CancelModal";
@@ -23,6 +26,8 @@ import SVG_PENCIL from "@/assets/images/icons/edit.svg";
 import {CardData} from "@/app/types/billing";
 import {SavedCard} from "@/app/types/billing";
 
+const PX_CN = 'px-[min(2.7dvw,0.625rem)]';
+const Hr = <hr className={'border-control-white-d0 mt-[min(2.7dvw,0.81rem)] mb-[min(2.7dvw,1.57rem)]'}/>;
 
 const SUBSCRIPTIONS_TEMPLATE: Subscription[] = [
     {
@@ -266,6 +271,17 @@ function ManageSubscriptionsPage() {
         );
     }
 
+    const InvoiceRows: ReactElement[] = invoices.map((order, idx) => {
+        const invoiceDate = new Date(order.startDate);
+        return (
+            <tr key={idx}>
+                <td>{invoiceDate.toLocaleString('default', {month: 'long'})} {invoiceDate.getDate()}th, {invoiceDate.getFullYear()}</td>
+                <td className={'sm:hidden'}>${order.amount}</td>
+                <td className={'text-right sm:hidden'}>{order.name}</td>
+            </tr>
+        )
+    });
+
     return (
         <div className={'pt-[9.14rem] px-[1.83rem]'}>
         <h1 className={'text-[3rem] font-bold mb-[3.12rem]'}>
@@ -310,6 +326,23 @@ function ManageSubscriptionsPage() {
         </div>
             {RenderPlanInfo()}
             <ScrollEnd/>
+            <div className={`flex justify-between items-center mt-[90px]`}>
+                <h2 className={'text-header font-bold text-left'}>Invoice History</h2>
+                <Button
+                    className={'border-small border-control-white-d0 px-[min(2.1dvw,1rem)] text-small h-[min(4.3dvw,1.44rem)] rounded-full font-bold'}
+                    onClick={() => modalCtx.openModal(<ExportInvoiceModal/>, {darkenBg: true})}
+                >
+                    Export
+                </Button>
+            </div>
+            {Hr}
+            <div className={'overflow-hidden rounded-small px-[min(4dvw,var(--p-small))] max-h-[27rem]'}>
+                <div className={`overflow-y-scroll h-full text-[min(3.2dvw,var(--fz-content-))] capitalize`}>
+                    <table className={'w-full'} cellPadding={'1.25'}>
+                        <tbody>{InvoiceRows}</tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 }
