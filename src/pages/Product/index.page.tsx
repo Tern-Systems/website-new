d6aa997ba9d0d23fcf6dc2caf1700d0822ef64ad
Maksym Shapovalov <maksym.shapovalovv@gmail.com>
2024-12-29 18:25:25 -0500

    feat: implement sm:landscape for Product page, fixes
    - fix var name in common.module.css

--- src/pages/Product/index.page.tsx
+++ src/pages/Product/index.page.tsx
@@ -7,16 +7,23 @@ import styles from "@/app/common.module.css";
 
 
 const TernKeyPage: FC = () => (
-    <div className={`${styles.highlight} w-[min(90dvw,33rem)] place-items-center [&&]:mx-auto [&&]:text-center`}>
+    <div className={`${styles.highlight} place-items-center mx-auto w-[min(90dvw,33rem)] text-center
+                    sm:landscape:x-[grid,grid-rows-2,grid-cols-2]`}
+    >
         <span className={'text-[min(9.6dvw,3.75rem)] font-oxygen font-bold'}>TernKey</span>
-        <a href={"https://www.tern.ac/ternkey/"} target={'_blank'}>
+        <a href={"https://www.tern.ac/ternkey/"}
+           target={'_blank'}
+           className={'my-[min(5.3dvw,3.15rem)] sm:landscape:x-[row-span-2,my-0]'}
+        >
             <Image
                 src={SVG_INSIGNIA}
                 alt={'insignia'}
-                className={'h-[55dvw] max-h-[16rem] my-[min(5.3dvw,3.15rem)]'}
+                className={'h-[55dvw] max-h-[min(20dvw,16rem)]'}
             />
         </a>
-        <span className={'text-section-header'}>Unlocking the potential of ternary programming.</span>
+        <span className={'text-section-header sm:landscape:text-content-small'}>
+            Unlocking the potential of ternary programming.
+        </span>
     </div>
 );
 
