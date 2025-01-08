4d58b72a8b024ce16ca2ee3c437cd751e9d80112
Maksym Shapovalov <maksym.shapovalovv@gmail.com>
2025-01-08 12:20:09 -0500

    fix: fix env vars reading in BaseService

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,18 +1,15 @@
 abstract class BaseService {
-    protected readonly _env: string;
     protected readonly _API: string;
     protected readonly _serviceName: string;
 
     protected constructor(name: string) {
-        const env: string | undefined = process.env.NEXT_PUBLIC_NODE_ENV ?? process.env.NODE_ENV;
-
+        const env: string | undefined = process.env.NEXT_PUBLIC_NODE_ENV ?? process.env.NODE_ENV ?? 'development';
         if (!env)
             this.debug('No ENV set, continuing in development mode...');
 
-        this._env = env ?? 'development';
-        const api: string | undefined = this._env === 'development' ? process.env.NEXT_PUBLIC_API_DEV : process.env.NEXT_PUBLIC_API;
+        const api: string | undefined = env === 'development' ? process.env.NEXT_PUBLIC_API_DEV : process.env.NEXT_PUBLIC_API;
 
-        this.debug('ENV:', this._env + ',', 'API url:', api);
+        this.debug('ENV:', env + ',', 'API url:', api);
         if (!api)
             throw 'API URL is not defined!'
 
@@ -21,18 +18,20 @@ abstract class BaseService {
     }
 
     protected getLoggers(method: string) {
-        console.log(this._serviceName + ' - ' + method + ':')
+        console.log(this._serviceName + ' - ' + method + ':');
         return [this.debug, this.error];
     }
 
     // eslint-disable-next-line
     private debug(...data: any[]): void {
-        if (this._env === 'development')
+        const env: string | undefined = process.env.NEXT_PUBLIC_NODE_ENV ?? process.env.NODE_ENV ?? 'development';
+        if (env === 'development')
             console.log('DEBUG', ...data);
     }
 
     private error(error: unknown) {
-        if (this._env === 'development')
+        const env: string | undefined = process.env.NEXT_PUBLIC_NODE_ENV ?? process.env.NODE_ENV ?? 'development';
+        if (env === 'development')
             console.log('ERROR', error);
     }
 }
