60eb31de2ba0a529083f5a2418554b0f6def63df
Maksym Shapovalov <maksym.shapovalovv@gmail.com>
2025-01-08 10:06:02 -0500

    fix: fix API url

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,11 +1,21 @@
 abstract class BaseService {
+    protected readonly _env: string;
     protected readonly _API: string;
     protected readonly _serviceName: string;
 
     protected constructor(name: string) {
-        const api: string | undefined = process.env.NEXT_PUBLIC_API;
+        const env: string | undefined = process.env.NEXT_PUBLIC_NODE_ENV ?? process.env.NODE_ENV;
+
+        if (!env)
+            this.debug('No ENV set, continuing in development mode...');
+
+        this._env = env ?? 'development';
+        const api: string | undefined = this._env === 'development' ? process.env.NEXT_PUBLIC_API_DEV : process.env.NEXT_PUBLIC_API;
+
+        this.debug('ENV:', this._env + ',', 'API url:', api);
         if (!api)
             throw 'API URL is not defined!'
+
         this._API = api;
         this._serviceName = name;
     }
@@ -17,12 +27,12 @@ abstract class BaseService {
 
     // eslint-disable-next-line
     private debug(...data: any[]): void {
-        if (process.env.NODE_ENV === 'development')
+        if (this._env === 'development')
             console.log('DEBUG', ...data);
     }
 
     private error(error: unknown) {
-        if (process.env.NODE_ENV === 'development')
+        if (this._env === 'development')
             console.log('ERROR', error);
     }
 }
