87847daf2f35e1caf59b0e584cd02bdc7db6ee75
Maksym Shapovalov <maksym.shapovalovv@gmail.com>
2025-01-11 17:20:29 -0500

    fix: fix casing for page routes

---
+++ src/pages/all_ways/TBD/index.page.tsx
@@ -0,0 +1,17 @@
+import {FC} from "react";
+
+import styles from "@/app/common.module.css";
+
+
+const AllWaysPage: FC = () => (
+    <div className={`${styles.highlight} w-[min(90dvw,33rem)] place-items-center [&&]:mx-auto [&&]:text-center`}>
+<<<<<<<< Updated upstream:src/pages/all_ways/TBD/index.page.tsx
+        <span className={'text-[min(9.6dvw,3.75rem)] font-oxygen font-bold'}>All Ways</span>
+========
+        <span className={'text-[min(9.6dvw,3.75rem)] text-nowrap font-oxygen font-bold'}>All Products Page</span>
+>>>>>>>> Stashed changes:src/pages/Products/index.page.tsx
+    </div>
+);
+
+
+export default AllWaysPage;
