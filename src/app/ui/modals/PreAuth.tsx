da083891b0f8e5c246ae7ff8e08337685e6cb2a2
Maksym Shapovalov <maksym.shapovalovv@gmail.com>
2025-01-10 19:21:02 -0500

    fix: fix PreAuthModal up to figma specs

--- src/app/ui/modals/PreAuth.tsx
+++ src/app/ui/modals/PreAuth.tsx
@@ -5,6 +5,7 @@ import {useModal} from "@/app/context";
 import {BaseModal} from "@/app/ui/modals/Base";
 import {AuthModal} from "@/app/ui/modals/Auth";
 import {Button} from "@/app/ui/form";
+import cn from "classnames";
 
 
 const PreAuthModal: FC = () => {
@@ -14,29 +15,33 @@ const PreAuthModal: FC = () => {
         <BaseModal
             smScreenOnly
             title={'Tern Account'}
-            classNameTitle={'place-self-start   sm:landscape:place-self-start sm:landscape:ml-0'}
-            classNameContent={'place-items-center p-[1.25rem]'}
+            classNameTitle={'justify-self-start text-heading   sm:landscape:ml-0'}
+            classNameContent={cn(
+                'w-full items-start mx-auto px-[--p-content-xs] py-[--p-content] place-items-center text-basic',
+                'max-w-[23rem]',
+                'sm:landscape:x-[max-w-[73rem],px-[--p-content-3xl]]'
+            )}
         >
-            <div className={'place-items-center     w-full     sm:landscape:flex sm:landscape:justify-between'}>
+            <div className={'flex flex-col w-full  sm:landscape:flex-row sm:landscape:justify-between'}>
                 <div>
                     <p>Your Tern account provides you with:</p>
-                    <ul className={'flex flex-col   gap-y-[1.25rem] mt-[1.25rem] mb-[1.87rem]   list-disc list-inside'}>
+                    <ul className={'flex flex-col   gap-y-[--p-content-xs] mt-[--p-content-xs] mb-[--p-content]   list-disc list-inside'}>
                         <li>Single sign-on to the Tern ecosystem</li>
                         <li>Personalized recommendations</li>
                         <li>Test drives and other trials</li>
                         <li>And many more exclusive benefits</li>
                     </ul>
                 </div>
-                <div className={'px-[1rem] max-w-[19rem] w-full     font-bold text-small'}>
+                <div className={'place-self-center max-w-[19rem] w-full sm:portrait:w-[85%] font-bold text-section-s'}>
                     <Button
                         onClick={() => modalCtx.openModal(<AuthModal isLoginAction/>)}
-                        className={'mb-[0.94rem] w-full h-[2.7rem]      rounded-full bg-control-blue  text-primary'}
+                        className={'mb-[--p-content-xxs] w-full h-[2.7rem]      rounded-full bg-control-blue  text-primary'}
                     >
                         Login
                     </Button>
                     <Button
                         onClick={() => modalCtx.openModal(<AuthModal/>)}
-                        className={'w-full h-[2.7rem]   border-small rounded-full border-control-blue'}
+                        className={'w-full h-[2.7rem] border-small rounded-full border-control-blue'}
                     >
                         Sign Up
                     </Button>
