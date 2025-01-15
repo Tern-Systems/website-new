1359b39b170cb3a3273bc7e1337a8cd89ceccc17
Maksym Shapovalov <maksym.shapovalovv@gmail.com>
2025-01-15 09:45:59 -0500

    fix: fix mapSavedCard util, fix page name for TernKey subscribe

--- src/app/utils/payment.ts
+++ src/app/utils/payment.ts
@@ -1,27 +1,27 @@
 import {CardData, SavedCardFull} from "@/app/types/billing";
 import {CountryKey, StateKey} from "@/app/static";
 
-const mapSavedCard = (card: SavedCardFull): CardData => {
-    const billingInfo = card.billingAddress;
-    const [addressLine1, addressLine2] = card?.billingAddress.address.split('|');
+const mapSavedCard = (card: SavedCardFull | undefined): CardData => {
+    const billingInfo = card?.billingAddress;
+    const [addressLine1, addressLine2] = card?.billingAddress?.address?.split('|') ?? [];
     return {
-        id: card.paymentProfileId,
-        profileId: card.customerProfileId,
-        cardNumber: card.cardNumber,
-        billingAddress: card?.billingAddress.address,
-        nickName: card.nickName,
-        type: card.cardType,
+        id: card?.paymentProfileId ?? '',
+        profileId: card?.customerProfileId ?? '',
+        cardNumber: card?.cardNumber ?? '',
+        billingAddress: card?.billingAddress?.address ?? '',
+        nickName: card?.nickName ?? '',
+        type: card?.cardType ?? '',
         cvc: '',
-        expirationDate: card.expDate,
-        cardholderName: billingInfo.firstName + ' ' + billingInfo.lastName,
+        expirationDate: card?.expDate ?? '',
+        cardholderName: (billingInfo?.firstName ?? '') + ' ' + (billingInfo?.lastName ?? ''),
         addressLine1: addressLine1 ?? '',
         addressLine2: addressLine2 ?? '',
-        city: billingInfo.city,
-        state: billingInfo.state as StateKey,
-        zip: billingInfo.zip,
-        country: billingInfo.country as CountryKey,
-        isPreferred: card.preferred,
+        city: billingInfo?.city ?? '',
+        state: billingInfo?.state as StateKey,
+        zip: billingInfo?.zip ?? '',
+        country: billingInfo?.country as CountryKey,
+        isPreferred: card?.preferred ?? false,
     }
 }
 
-export  {mapSavedCard}
+export {mapSavedCard}
