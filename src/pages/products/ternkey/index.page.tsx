087fe41e822100feb36829f0eb59f19f58057ccf
Jomar Tabios Granada <jomart.granada@gmail.com>
2025-01-16 22:09:35 +0800

    feat: add auto login to ternkey

--- src/pages/products/ternkey/index.page.tsx
+++ src/pages/products/ternkey/index.page.tsx
@@ -5,27 +5,31 @@ import SVG_INSIGNIA from "/public/images/insignia.svg";
 
 import styles from "@/app/common.module.css";
 
+import { useUser } from "@/app/context";
 
-const TernKeyPage: FC = () => (
-    <div className={`${styles.highlight} place-items-center mx-auto w-[min(90dvw,33rem)] text-center
-                    sm:landscape:x-[grid,grid-rows-2,grid-cols-2]`}
-    >
-        <span className={'text-[min(9.6dvw,3.75rem)] font-oxygen font-bold'}>TernKey</span>
-        <a href={"https://www.tern.ac/ternkey/"}
-           target={'_blank'}
-           className={'my-[min(5.3dvw,3.15rem)] sm:landscape:x-[row-span-2,my-0]'}
+const TernKeyPage: FC = () => {
+    const userCtx = useUser();
+    return (
+        <div className={`${styles.highlight} place-items-center mx-auto w-[min(90dvw,33rem)] text-center
+                        sm:landscape:x-[grid,grid-rows-2,grid-cols-2]`}
         >
-            <Image
-                src={SVG_INSIGNIA}
-                alt={'insignia'}
-                className={'w-auto h-[55dvw] max-h-[min(20dvw,16rem)] my-[min(5.3dvw,3.15rem)]'}
-            />
-        </a>
-        <span className={'text-section-header sm:landscape:text-content-small'}>
-            Unlocking the potential of ternary programming.
-        </span>
-    </div>
-);
+            <span className={'text-[min(9.6dvw,3.75rem)] font-oxygen font-bold'}>TernKey</span>
+            <a href={userCtx.token ? `http://www.tern.ac/ternkey?website_login=${encodeURIComponent(userCtx.token || '')}` : `http://www.tern.ac/ternkey/`}
+            target={'_blank'}
+            className={'my-[min(5.3dvw,3.15rem)] sm:landscape:x-[row-span-2,my-0]'}
+            >
+                <Image
+                    src={SVG_INSIGNIA}
+                    alt={'insignia'}
+                    className={'w-auto h-[55dvw] max-h-[min(20dvw,16rem)] my-[min(5.3dvw,3.15rem)]'}
+                />
+            </a>
+            <span className={'text-section-header sm:landscape:text-content-small'}>
+                Unlocking the potential of ternary programming.
+            </span>
+        </div>
+    )
+};
 
 
 export default TernKeyPage;
