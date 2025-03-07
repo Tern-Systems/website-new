6a966a198e271f0244d1cbd282f524e0990c94bb
Jomar Tabios Granada <jomart.granada@gmail.com>
2025-03-07 22:21:34 +0800

    fix: rename remaining ternkey instances

--- src/app/types/subscription.ts
+++ src/app/types/subscription.ts
@@ -1,7 +1,7 @@
 import { Route } from '@/app/static';
 
 type SubscriptionRecurrency = 'annual' | 'monthly';
-type PlanName = 'Tidal' | 'trial';
+type PlanName = 'Tidal' | 'trial' | 'TernKey';
 type PlanType = 'Basic' | 'Pro';
 
 type SubscriptionBase = {
