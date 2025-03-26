b43f23540e2b568b4fa4f9c59282271b58f4a6b3
Maksym Shapovalov <maksym.shapovalovv@gmail.com>
2025-03-26 13:47:16 -0400

    config: update app build / test configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['page.tsx', 'jsx', '_app.tsx'],
-    images: { // TODO
-        unoptimized: true,
+    pageExtensions: ['tsx', 'page.tsx', 'jsx', '_app.tsx', 'js', 'ts'],
+    swcMinify: true,
+    experimental: {
+        forceSwcTransforms: true,
+        swcTraceProfiling: true,
+    },
+    webpack(config, { dev }) {
+        if (dev) config.cache = true;
+        return config;
+    },
+    compiler: {
+        removeConsole: { exclude: ['log'] },
+    },
+    images: {
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
         remotePatterns: [
             {
-                hostname: '127.0.0.1',
-                port: '8000',
+                protocol: 'https',
+                hostname: 'localhost',
+                pathname: '/**',
             },
             {
                 protocol: 'https',
                 hostname: 'storage.googleapis.com',
+                pathname: '/**',
             },
         ],
+        loader: 'default',
     },
 };
 
