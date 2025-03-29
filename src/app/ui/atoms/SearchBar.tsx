1a634710b06c3fec972791b329d50f05ca567960
Jomar Tabios Granada <jomart.granada@gmail.com>
2025-03-29 17:32:41 +0800

    fix: icons import path

--- src/app/ui/atoms/SearchBar.tsx
+++ src/app/ui/atoms/SearchBar.tsx
@@ -4,9 +4,9 @@ import cn from 'classnames';
 
 import { Input, Select } from '../form';
 
-import SVG_ARROW_DOWN from '/public/images/icons/arrow-down.svg';
-import SVG_SEARCH from '/public/images/icons/search.svg';
-import SVG_CALENDAR from '/public/images/icons/calendar.svg';
+import SVG_ARROW_DOWN from '@/assets/images/icons/arrow-down.svg';
+import SVG_SEARCH from '@/assets/images/icons/search.svg';
+import SVG_CALENDAR from '@/assets/images/icons/calendar.svg';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
     contentTypes: Record<string, string>;
