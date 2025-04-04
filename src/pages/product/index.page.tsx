53babc2988da18bd5be9a4f19bc891b9cd75fece
Tamir Yirga <tkifle@gmail.com>
2025-04-04 12:11:22 -0700

    Revert "Corrected gradient placement"
    This reverts commit 9595e3c7f694144bf629a7dfaa0ad1593f4b4028.

--- src/pages/product/index.page.tsx
+++ src/pages/product/index.page.tsx
@@ -154,7 +154,7 @@ function TidalPage() {
             </section>
             <section
                 style={{ backgroundImage: `url("${PNG_BACKGROUND_CIRCUIT.src}")` }}
-                className={cn(styles.section, styles.sectionBlueShadowBelow)}
+                className={cn(styles.section, styles.sectionShadowBlack)}
             >
                 <div
                     className={cn(
