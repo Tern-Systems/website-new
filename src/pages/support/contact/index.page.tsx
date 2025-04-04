a875f4f0a6448eb0c4d2113d2766234a739a7f9f
Tamir Yirga <tkifle@gmail.com>
2025-04-03 17:07:29 -0700

    Used MainBackground component for the parallax effect

--- src/pages/support/contact/index.page.tsx
+++ src/pages/support/contact/index.page.tsx
@@ -17,6 +17,7 @@ import styles from '@/app/common.module.css';
 
 import OFFICE_GIRL_3 from '@/assets/images/office-girl-3.png';
 import PNG_HIGHLIGHTEDTIPS from '@/assets/images/contact-card-highlighted-0.png';
+import { MainBackground } from '@/app/ui/atoms/MainBackground';
 
 type FormData = {
     isAllowedUpdate: boolean | undefined;
@@ -68,18 +69,11 @@ const ContactsPage: FC = () => {
     return (
         <>
             <section className={'flex justify-center w-full'}>
-                <div
-                    className={cn(
-                        'h-dvh max-h-[62.5rem] w-full max-w-[120rem]',
-                        'relative bg-cover bg-center bg-fixed',
-                    )}
-                    style={{
-                        backgroundImage: `url(${OFFICE_GIRL_3.src})`,
-                        position: 'relative',
-                        backgroundSize: 'cover',
-                        backgroundPosition: '50% top',
-                    }}
-                >
+                <div className={cn('h-dvh max-h-[62.5rem] w-full max-w-[120rem]')}>
+                    <MainBackground
+                        url={OFFICE_GIRL_3.src}
+                        className={cn('bg-top')}
+                    />
                     <div className={cn(styles.content, 'relative z-10 flex items-start justify-start')}>
                         <div>
                             <h1
