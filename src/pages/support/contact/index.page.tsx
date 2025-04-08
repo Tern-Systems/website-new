69878d405ae348772076f10b32b6de295320bfb3
Bryce Chimene <brycechimene@yahoo.com>
2025-04-07 20:24:11 -0500

    WB-126: Make Contact page banner fixed to match About and Tidal behavior

--- src/pages/support/contact/index.page.tsx
+++ src/pages/support/contact/index.page.tsx
@@ -11,6 +11,7 @@ import { CardLink, ResourceSectionData } from '@/app/types/layout';
 import { ResourceCard } from '@/app/ui/organisms';
 import { ResourcesSection } from '@/app/ui/templates';
 import { PageLink } from '@/app/ui/layout';
+import { MainBackground } from '@/app/ui/atoms';
 import { Route } from '@/app/static';
 
 import styles from '@/app/common.module.css';
@@ -67,16 +68,8 @@ const ContactsPage: FC = () => {
 
     return (
         <>
-            <section className={'flex justify-center w-full'}>
-                <div
-                    className={cn('h-dvh max-h-[62.5rem] w-full max-w-[120rem]', 'relative bg-cover bg-center')}
-                    style={{
-                        backgroundImage: `url(${OFFICE_GIRL_3.src})`,
-                        position: 'relative',
-                        backgroundSize: 'cover',
-                        backgroundPosition: '50% top',
-                    }}
-                >
+            <section className={cn(styles.section, styles.fullHeightSection)}>
+                <MainBackground url={OFFICE_GIRL_3.src} />
                     <div className={cn(styles.content, 'relative z-10 flex items-start justify-start')}>
                         <div>
                             <h1
@@ -94,7 +87,6 @@ const ContactsPage: FC = () => {
                     </div>
                     <div className='absolute inset-0 bg-gradient-to-r from-black via-black via-0% lg:via-5% to-transparent  sm:to-60%  md:to-40% lg:to-50% z-0' />
                     <div className='absolute inset-0 bg-gradient-to-l from-black from-0%   via-black via-0% lg:via-10%   to-transparent to-0% lg:to-20% z-1' />
-                </div>
             </section>
 
             <div
