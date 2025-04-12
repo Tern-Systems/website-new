8f8ae90eb39acaa5ae032f92a63a9f0ba141d9e3
G-Beast <tanmaysaxena58@gmail.com>
2025-04-12 19:46:30 -0400

    tidal: fixed fadeout issue

--- src/pages/product/index.page.tsx
+++ src/pages/product/index.page.tsx
@@ -154,7 +154,7 @@ function TidalPage() {
             </section>
             <section
                 style={{ backgroundImage: `url("${PNG_BACKGROUND_CIRCUIT.src}")` }}
-                className={styles.section}
+                className={styles.handbookSection}
             >
                 <div
                     className={cn(
