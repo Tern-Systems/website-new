353c6a66dfba657dedc5a3bf3b0e67b9e53210a0
Ahmad Searcy <ahmadsearcy7@gmail.com>
2025-04-29 13:32:17 -0500

    refactored Contacts page to leverage the MainBackground component rather than hardcording background style

--- src/pages/support/contact/index.page.tsx
+++ src/pages/support/contact/index.page.tsx
@@ -70,6 +70,7 @@ const ContactsPage: FC = () => {
         <>
             <section className={'flex justify-center w-full'}>
                 <MainBackground url={OFFICE_GIRL_3.src} />
+                {/* edited */}
                 <div className={cn('h-dvh max-h-[62.5rem] w-full max-w-[120rem]', 'relative bg-cover bg-center')}>
                     <div className={cn(styles.content, 'relative z-10 flex items-start justify-start')}>
                         <div>
