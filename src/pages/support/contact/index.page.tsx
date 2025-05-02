6d9d06f21d58a70ea224136bde418456b78b3bb7
Ben Booth <benpaulbooth@gmail.com>
2025-05-02 15:33:23 -0500

    Fixed the Contact page to remain fixed while the user scrolls up.

--- src/pages/support/contact/index.page.tsx
+++ src/pages/support/contact/index.page.tsx
@@ -69,12 +69,13 @@ const ContactsPage: FC = () => {
         <>
             <section className={'flex justify-center w-full'}>
                 <div
-                    className={cn('h-dvh max-h-[62.5rem] w-full max-w-[120rem]', 'relative bg-cover bg-center')}
+                    className={cn('h-dvh max-h-[62.5rem] w-full max-w-[120rem]', 'relative bg-cover bg-center bg-fixed')}
                     style={{
                         backgroundImage: `url(${OFFICE_GIRL_3.src})`,
                         position: 'relative',
                         backgroundSize: 'cover',
                         backgroundPosition: '50% top',
+                        backgroundAttachment: 'fixed',
                     }}
                 >
                     <div className={cn(styles.content, 'relative z-10 flex items-start justify-start')}>
