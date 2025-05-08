37e1c711e87f91887eddfbf2829138384047925d
Kaile Zheng <116919799+MudkipzRawsme@users.noreply.github.com>
2025-05-07 20:44:14 -0400

    BTMC tab creation

--- BTMC/btmc/src/ui/forms/main/tabs/T_BTMC/BTMC.xaml.cs
+++ BTMC/btmc/src/ui/forms/main/tabs/T_BTMC/BTMC.xaml.cs
@@ -1,51 +1,60 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using System.Collections.Generic;
-
-using btmc.src.alu;
-
-using toolbox = btmc.src.ui.resources.toolbox;
-using btmc.src.ui.forms.main.components;
 
 namespace btmc.src.ui.forms.main.tabs.T_BTMC
 {
-    using TypeEntry = KeyValuePair<Utils.TypeEnum, string>;
-
-    /// <summary>
-    /// Interaction logic for BTMC.xaml
-    /// </summary>
     public partial class BTMC : UserControl
     {
-        public Converter.TypeDict Types { get; }
+        public static readonly DependencyProperty InputOperationProperty =
+            DependencyProperty.Register("InputOperation", typeof(object), typeof(BTMC),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty OutputOperationProperty =
+            DependencyProperty.Register("OutputOperation", typeof(object), typeof(BTMC),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty InputValueProperty =
+            DependencyProperty.Register("InputValue", typeof(string), typeof(BTMC),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty OutputValueProperty =
+            DependencyProperty.Register("OutputValue", typeof(string), typeof(BTMC),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public object InputOperation
+        {
+            get { return GetValue(InputOperationProperty); }
+            set { SetValue(InputOperationProperty, value); }
+        }
+
+        public object OutputOperation
+        {
+            get { return GetValue(OutputOperationProperty); }
+            set { SetValue(OutputOperationProperty, value); }
+        }
+
+        public string InputValue
+        {
+            get { return (string)GetValue(InputValueProperty); }
+            set { SetValue(InputValueProperty, value); }
+        }
+
+        public string OutputValue
+        {
+            get { return (string)GetValue(OutputValueProperty); }
+            set { SetValue(OutputValueProperty, value); }
+        }
 
         public BTMC()
         {
             InitializeComponent();
-            Types = Converter.Type;
-            DataContext = this;
         }
 
-
-        private void BtnSwap_Click(object sender, RoutedEventArgs e)
+        private void SwapButton_SwapClicked(object sender, RoutedEventArgs e)
         {
-            // Implement it (make sure to reuse as much code as its possible)
-        }
-
-        private void CmbTypeIn_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            /* Uncomment after you add the required components to the form */
-            //_HandleSelectionChanged(ref cmbBTMC_Out, ref cmbBTMC_In, ref tbBTMC_In);
-        }
-
-        private void CmbTypeOut_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            /* Uncomment after you add the required components to the form */
-            //_HandleSelectionChanged(ref cmbBTMC_In, ref cmbBTMC_Out, ref tbBTMC_Out);
-        }
-
-        private void _HandleSelectionChanged(ref toolbox.ComboBox cmb0, ref toolbox.ComboBox cmb1, ref toolbox.TextBox tb)
-        {
-            // Implement it
+            var temp = InputOperation;
+            InputOperation = OutputOperation;
+            OutputOperation = temp;
         }
     }
 }
