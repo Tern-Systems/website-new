bc7a03ce407710028419618b378a9106b21f9a6c
Jing <jngo1@stevens.edu>
2025-06-16 16:04:11 -0400

    populated courses page and created courses library

--- src/pages/support/training/courses/index.page.tsx
+++ src/pages/support/training/courses/index.page.tsx
@@ -5,88 +5,18 @@ import { useEffect, useState } from 'react';
 import { Content, H1, H2, Section } from '@/app/ui/atoms';
 import { Route } from '@/app/static';
 
-import ABSTRACT_DATA_STREAM from '@/assets/images/abstract-data-stream.jpg';
 import { BookCoverSection } from '@/app/ui/templates';
 import { CardsLibrary } from '@/app/ui/templates/Sections/CardsLibrary';
-import { Course } from '@/app/types/course';
+import { Course, COURSES_TEMPLATE } from '@/app/types/course';
 import { InsideTernSection } from '@/app/ui/templates/Sections/InsideTern';
-
-const COURSE_LINKS: Course[] = [
-    {
-        title: 'Tidal',
-        description: 'Creating a Profile, Explore Keys, Favorites, Key Creation, Save Features, My Keys, Key Editor',
-        id: 'tidal-1',
-        category: 'Individual',
-        content: '',
-        thumbnail: '',
-        date: Date.now(),
-        label: 'Course',
-        durationMs: 3600000,
-        subject: 'Tidal',
-        tag: 'popular',
-        type: 'text',
-    },
-    {
-        title: 'G25',
-        description:
-            'Writing Your First G Program, Syntax Rules, Language Overview, Literals, Data Type Specifiers, Keywords, State Operators',
-        id: 'g25-1',
-        category: 'Individual',
-        content: '',
-        thumbnail: '',
-        date: Date.now(),
-        label: 'Course',
-        durationMs: 3600000,
-        subject: 'G25',
-        tag: 'featured',
-        type: 'text',
-    },
-    {
-        title: 'T27I',
-        description:
-            'How to Read TERN, Syntax Rules, Language Overview, Instructions, Arithmetic, Branching, Tertwise, Opcode Meanings',
-        id: 't27i-1',
-        category: 'Individual',
-        content: '',
-        thumbnail: '',
-        date: Date.now(),
-        label: 'Course',
-        durationMs: 3600000,
-        subject: 'T27I',
-        tag: 'popular',
-        type: 'text',
-    },
-    {
-        title: 'BTMC',
-        description:
-            'Balanced Ternary Logic, Basic Arithmetic, Language Structure, Heptavintimal Encoding, Advanced Arithmetic, UEF',
-        id: 'btmc-1',
-        category: 'Individual',
-        content: '',
-        thumbnail: '',
-        date: Date.now(),
-        label: 'Course',
-        durationMs: 3600000,
-        subject: 'BTMC',
-        tag: 'featured',
-        type: 'text',
-    },
-];
+import PNG_COURSES_BG from '@/assets/images/courses-bg-main.png';
 
 function CoursesPage() {
-    const [courses, setCourses] = useState<Course[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        setCourses(COURSE_LINKS);
-        setIsLoading(false);
-    }, []);
-
     return (
         <>
             <Section
                 type={'short'}
-                background={{ image: ABSTRACT_DATA_STREAM, gradient: 'left' }}
+                background={{ image: PNG_COURSES_BG, gradient: 'left' }}
                 className={{
                     content: 'flex flex-col justify-between  py-xxl sm:pb-[2.81rem]',
                     background: 'sm:bg-center',
@@ -111,8 +41,8 @@ function CoursesPage() {
                         first: { title: 'Most Popular', href: Route.AllWays },
                         second: { title: 'Featured', href: Route.AllWays },
                     }}
-                    cards={COURSE_LINKS}
-                    tags={['popular', 'featured', 'popular', 'featured']}
+                    cards={COURSES_TEMPLATE}
+                    tags={['Popular', 'Featured', 'Free', 'Premium']}
                 />
                 <BookCoverSection
                     className='sm:h-[62rem] bookcover-section sm:[&_h3]:!mt-[7rem] sm:[&_h3]:!self-start'
